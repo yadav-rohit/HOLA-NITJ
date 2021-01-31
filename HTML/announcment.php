88355bc6a20f9ea7adfaa1014e4695664579e4d4
Rohit <49914319+yadav-rohit@users.noreply.github.com>
2021-01-31 11:05:40 +0530

    php second wave

---
+++ HTML/announcment.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="CSS/an.css">
+    </head>
+    <body class="bodies teo">
+          <?php include "navbar.html"?>
+        </div>
+    </body>
+</html>
---
+++ HTML/q&a.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="CSS/bks.css">
+    </head>
+    <body>
+          <?php include "navbar.html"?>
+        </div>
+    </body>
+</html>
